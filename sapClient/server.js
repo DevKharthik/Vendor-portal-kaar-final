217a35045e05e8b55477822936712e396bddccee
DevKharthik <devkharthik@gmail.com>
2025-09-08 15:09:22 +0530

    good commit

--- sapClient/server.js
+++ sapClient/server.js
@@ -21,7 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use(cors());
 app.use(bodyParser.json());
 
-// Route mounting
+// Route mounting in server
 app.use('/api/vendor/login', vendorLoginRoute);  // if you move login to route file
 app.use('/api/vendor/profile', vendorProfileRoute);
 app.use('/api/vendor/rfq', rfqRoute);
